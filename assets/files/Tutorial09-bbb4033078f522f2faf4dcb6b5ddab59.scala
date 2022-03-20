404b3780aefa237cd296dbd01eadd6104aba67ef
Marcel Luethi <marcel.luethi@unibas.ch>
2022-03-20 19:15:44 +0100

    Deploy website - based on bb49385284d196bcbfa32c1fd6bd9e933a4693f4

---
+++ assets/files/Tutorial09-bbb4033078f522f2faf4dcb6b5ddab59.scala
@@ -0,0 +1,85 @@
+//> using scala "2.13"
+//> using lib "ch.unibas.cs.gravis::scalismo-ui:0.91-RC1"
+import scalismo.geometry._
+import scalismo.common._
+import scalismo.common.interpolation.TriangleMeshInterpolator3D
+import scalismo.mesh._
+import scalismo.io.{StatisticalModelIO, MeshIO, LandmarkIO}
+import scalismo.statisticalmodel._
+import scalismo.numerics.UniformMeshSampler3D
+import scalismo.kernels._
+import breeze.linalg.{DenseMatrix, DenseVector}
+
+import scalismo.ui.api._
+
+object Tutorial9 extends App {
+
+  scalismo.initialize()
+  implicit val rng: scalismo.utils.Random = scalismo.utils.Random(42)
+
+  val ui = ScalismoUI()
+
+  val noseless = MeshIO.readMesh(new java.io.File("datasets/noseless.ply")).get
+
+  val targetGroup = ui.createGroup("target")
+  ui.show(targetGroup, noseless, "noseless")
+
+  val smallModel =
+    StatisticalModelIO.readStatisticalTriangleMeshModel3D(new java.io.File("datasets/model.h5")).get
+
+  val scalarValuedKernel = GaussianKernel3D(70) * 10.0
+
+  case class XmirroredKernel(kernel: PDKernel[_3D]) extends PDKernel[_3D] {
+    override def domain = EuclideanSpace3D
+    override def k(x: Point[_3D], y: Point[_3D]) = kernel(Point(x(0) * -1f, x(1), x(2)), y)
+  }
+
+  def symmetrizeKernel(kernel: PDKernel[_3D]): MatrixValuedPDKernel[_3D] = {
+    val xmirrored = XmirroredKernel(kernel)
+    val k1 = DiagonalKernel(kernel, 3)
+    val k2 = DiagonalKernel(xmirrored * -1f, xmirrored, xmirrored)
+    k1 + k2
+  }
+
+  val gp = GaussianProcess[_3D, EuclideanVector[_3D]](symmetrizeKernel(scalarValuedKernel))
+
+  val lowrankGP = LowRankGaussianProcess.approximateGPCholesky(
+    smallModel.reference,
+    gp,
+    relativeTolerance = 0.5e-1,
+    interpolator = TriangleMeshInterpolator3D[EuclideanVector[_3D]]()
+  )
+  val model = PointDistributionModel.augmentModel(smallModel, lowrankGP)
+
+  val modelGroup = ui.createGroup("face model")
+  val ssmView = ui.show(modelGroup, model, "model")
+
+  val referenceLandmarks =
+    LandmarkIO.readLandmarksJson3D(new java.io.File("datasets/modelLandmarks.json")).get
+  val referencePoints: Seq[Point[_3D]] = referenceLandmarks.map(lm => lm.point)
+  val referenceLandmarkViews = referenceLandmarks.map(lm => ui.show(modelGroup, lm, s"lm-${lm.id}"))
+
+  val noselessLandmarks =
+    LandmarkIO.readLandmarksJson3D(new java.io.File("datasets/noselessLandmarks.json")).get
+  val noselessPoints: Seq[Point[_3D]] = noselessLandmarks.map(lm => lm.point)
+  val noselessLandmarkViews = noselessLandmarks.map(lm => ui.show(targetGroup, lm, s"lm-${lm.id}"))
+
+  val domain = UnstructuredPointsDomain3D(referencePoints.toIndexedSeq)
+  val deformations = (0 until referencePoints.size).map(i => noselessPoints(i) - referencePoints(i))
+  val defField = DiscreteField3D(domain, deformations)
+  ui.show(modelGroup, defField, "partial_Field")
+
+  val littleNoise =
+    MultivariateNormalDistribution(DenseVector.zeros[Double](3), DenseMatrix.eye[Double](3) * 0.5)
+
+  val regressionData = for ((refPoint, noselessPoint) <- referencePoints zip noselessPoints) yield {
+    val refPointId = model.reference.pointSet.findClosestPoint(refPoint).id
+    (refPointId, noselessPoint, littleNoise)
+  }
+
+  val posterior = model.posterior(regressionData.toIndexedSeq)
+
+  val posteriorGroup = ui.createGroup("posterior-model")
+  ui.show(posteriorGroup, posterior, "posterior")
+  ui.close()
+}
---
+++ assets/files/Tutorial14-c25106cf987204f727e66c5463844ad4.scala
@@ -0,0 +1,106 @@
+//> using scala "2.13"
+//> using lib "ch.unibas.cs.gravis::scalismo-ui:0.91-RC1"
+
+import scalismo.sampling.MHSample
+import scalismo.sampling.MHDistributionEvaluator
+import scalismo.sampling.evaluators.ProductEvaluator
+import scalismo.sampling.MHProposalGenerator
+import scalismo.sampling.proposals.GaussianRandomWalkProposal
+import scalismo.sampling.proposals.MHProductProposal
+import scalismo.sampling.ParameterConversion
+import scalismo.sampling.algorithms.MetropolisHastings
+import scalismo.sampling.loggers.MHSampleLogger
+import scalismo.sampling.proposals.MHMixtureProposal
+import scalismo.sampling.proposals.MHIdentityProposal
+import breeze.stats.meanAndVariance
+
+object Tutorial14 extends App {
+
+  scalismo.initialize()
+  implicit val rng: scalismo.utils.Random = scalismo.utils.Random(42)
+
+// We need this line to seed breeze's random number generator
+  implicit val randBasisBreeze: breeze.stats.distributions.RandBasis = rng.breezeRandBasis
+
+  val a = 0.2
+  val b = 3
+  val sigma2 = 0.5
+  val errorDist = breeze.stats.distributions.Gaussian(0, sigma2)(rng.breezeRandBasis)
+  val data = for (x <- 0 until 100) yield {
+    (x.toDouble, a * x + b + errorDist.draw())
+  }
+
+  case class Parameters(a: Double, b: Double, sigma2: Double)
+
+  implicit object tuple3ParameterConversion
+      extends ParameterConversion[Tuple3[Double, Double, Double], Parameters] {
+    def from(p: Parameters): Tuple3[Double, Double, Double] = (p.a, p.b, p.sigma2)
+    def to(t: Tuple3[Double, Double, Double]): Parameters =
+      Parameters(a = t._1, b = t._2, sigma2 = t._3)
+  }
+  case class LikelihoodEvaluator(data: Seq[(Double, Double)])
+      extends MHDistributionEvaluator[Parameters] {
+
+    override def logValue(theta: MHSample[Parameters]): Double = {
+
+      val likelihoods = for ((x, y) <- data) yield {
+        val likelihood = breeze.stats.distributions.Gaussian(
+          theta.parameters.a * x + theta.parameters.b,
+          theta.parameters.sigma2
+        )
+
+        likelihood.logPdf(y)
+      }
+      likelihoods.sum
+    }
+  }
+
+  object PriorEvaluator extends MHDistributionEvaluator[Parameters] {
+
+    val priorDistA = breeze.stats.distributions.Gaussian(0, 1)
+    val priorDistB = breeze.stats.distributions.Gaussian(0, 10)
+    val priorDistSigma = breeze.stats.distributions.LogNormal(0, 0.25)
+
+    override def logValue(theta: MHSample[Parameters]): Double = {
+      priorDistA.logPdf(theta.parameters.a)
+      +priorDistB.logPdf(theta.parameters.b)
+      +priorDistSigma.logPdf(theta.parameters.sigma2)
+    }
+  }
+  val posteriorEvaluator = ProductEvaluator(PriorEvaluator, LikelihoodEvaluator(data))
+
+  val genA = GaussianRandomWalkProposal(0.01, "rw-a-0.1").forType[Double]
+  val genB = GaussianRandomWalkProposal(0.05, "rw-b-0.5").forType[Double]
+  val genSigma = GaussianRandomWalkProposal(0.01, "rw-sigma-0.01").forType[Double]
+
+  val parameterGenerator = MHProductProposal(genA, genB, genSigma).forType[Parameters]
+
+  val identProposal = MHIdentityProposal.forType[Double]
+  val noiseOnlyGenerator =
+    MHProductProposal(identProposal, identProposal, genSigma).forType[Parameters]
+
+  val mixtureGenerator = MHMixtureProposal((0.1, noiseOnlyGenerator), (0.9, parameterGenerator))
+  val chain = MetropolisHastings(mixtureGenerator, posteriorEvaluator)
+
+  val logger = MHSampleLogger[Parameters]()
+  val initialSample = MHSample(Parameters(0.0, 0.0, 1.0), generatedBy = "initial")
+
+  val mhIterator = chain.iterator(initialSample, logger)
+  val samples = mhIterator.drop(1000).take(5000).toIndexedSeq
+  val meanAndVarianceA = meanAndVariance(samples.map(_.parameters.a))
+  println(
+    s"Estimates for parameter a: mean = ${meanAndVarianceA.mean}, var = ${meanAndVarianceA.variance}"
+  )
+  val meanAndVarianceB = meanAndVariance(samples.map(_.parameters.b))
+  println(
+    s"Estimates for parameter b: mean = ${meanAndVarianceB.mean}, var = ${meanAndVarianceB.variance}"
+  )
+  val meanAndVarianceSigma2 = meanAndVariance(samples.map(_.parameters.sigma2))
+  println(
+    s"Estimates for parameter sigma2: mean = ${meanAndVarianceSigma2.mean}, var = ${meanAndVarianceSigma2.variance}"
+  )
+  println("Acceptance ratios: " + logger.samples.acceptanceRatios)
+  println(
+    "acceptance ratios over the last 100 samples: " + logger.samples.takeLast(100).acceptanceRatios
+  )
+}
